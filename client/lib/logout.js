04439805f67955a7f681075e9b851cb9d8594f7e
goorancid <goorancid@hotmail.com>
2015-12-22 20:05:41 -0800

    Profile Page Tabs
    Working at putting in tabs and workable content, also deleted a lot of
    the admin theme photos for size crunch; working on pricing page, need to
    figure out pills

---
+++ client/lib/logout.js
@@ -0,0 +1,11 @@
+Template.topbar.events({
+    "click #logout": function (e, impl) {
+        Meteor.logout(function (err) {
+            if (err) {
+                // show err message
+            } else {
+                //show alert that says logged out
+            }
+        });
+    }
+});
